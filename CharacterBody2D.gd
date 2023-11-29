1412b46dec34ee63c2ac843f44a004e4dbb5250c
Manu <manulopez542@gmail.com>
2023-11-29 20:29:36 -0300

    Initial Commit
    Prototipo de pataformero en Godot 4.1.3

---
+++ CharacterBody2D.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animation_player = $AnimatedSprite2D
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		if velocity.y > 0:
+			animation_player.play("fall")
+		else:
+			animation_player.play("Jump")
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	animation_player.flip_h = 0 > direction if direction else animation_player.flip_h
+	if direction:
+		velocity.x = direction * SPEED
+		if is_on_floor():
+			animation_player.play("run")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			animation_player.play("idle")
+
+	move_and_slide()
