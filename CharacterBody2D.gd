e4c62c887ff93a594cf118ec34b09b0a49ec987d
Joaquin Druetta <joaquin.druetta@keylab.com.ar>
2025-01-25 01:42:11 -0300

    Se añade mecanica de jetpack, particles de jetpack, y shader de bubble

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var SPEED = 100.0
+var JETPACK_SPEED = 25 #25
+var FUEL = 1
+var JETPACK_ON = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -10,18 +12,21 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		if velocity.y > 0:
-			animation_player.play("fall")
-		else:
-			animation_player.play("Jump")
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		
+	$"fuelLabel".text = "FUEL: " + str(FUEL)
+	velocity.y += gravity * delta
+	# Handle Jetpack.
+	if Input.is_action_pressed("ui_up") && is_on_floor() && FUEL < 50:
+		#Cargamos FUEL
+		FUEL += 1
+	if Input.is_action_just_released("ui_up"):
+		JETPACK_ON = true
+	if JETPACK_ON && FUEL > 0:
+		velocity.y -= JETPACK_SPEED
+		FUEL -= 1
+		$"Jetpack Bubbles/CPUParticles2D".emitting = true
+	else:
+		JETPACK_ON = false
+		$"Jetpack Bubbles/CPUParticles2D".emitting = false		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
