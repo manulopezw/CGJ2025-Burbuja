f54b08029567836747ff2b595fd645ea887f5387
Manuel Lopez <manuel.lopez@keylab.com.ar>
2025-01-26 14:18:46 -0300

    nuevo tilemap de burbujas cargado

--- water_spring.gd
+++ water_spring.gd
@@ -39,7 +39,7 @@ func set_collision_width(value):
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.name == "Layer0":
+	if body.name == "TileMapLayer":
 		return
 	if body == colided_with:
 		return
